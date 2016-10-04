60c606457eddc3f43802914ceded246a32d46feb
A-Picogna <apicogna@enssat.fr>
2016-10-04 17:27:01 +0200

    Basic Setup

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -6,10 +6,10 @@ public class Map : MonoBehaviour {
 	public GameObject hexPrefab;
 
 	// size of map in terms of numer of hexagon
-	int width = 20;
-	int height = 20;
+	public int width = 20;
+	public int height = 20;
 	float xOffset = 0.882f;
-	float yOffset = 0.764f;
+	float zOffset = 0.764f;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +23,7 @@ public class Map : MonoBehaviour {
 					xPos += xOffset/2f;
 				}
 
-				GameObject hex_go = (GameObject) Instantiate (hexPrefab, new Vector3 (xPos, y*yOffset, 0), Quaternion.identity);
+				GameObject hex_go = (GameObject) Instantiate (hexPrefab, new Vector3 (xPos, 0, y*zOffset), Quaternion.identity);
 				hex_go.name = "Hex_" + x + "_" + y;
 				hex_go.transform.SetParent (this.transform);
 
---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// only work in orthographic mode :
+		// Vector3 worldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		// WITH 3D OBJECTS :
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (ray, out hitInfo)) {
+			Debug.Log (hitInfo.collider.transform.parent.name);
+		}
+
+	}
+}
