65c4560ca7bfe3516477a97a644c071ce247f129
Alexandre Picogna <apicogna@enssat.fr>
2016-10-09 17:08:31 +0200

    Selection et deplacement d une sphere

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -7,24 +7,35 @@ public class Hex : MonoBehaviour {
 	public int x;
 	public int y;
 
-	public Hex[] getNeighbours(){
-		
+	public GameObject[] getNeighbours(){
+
+		GameObject[] Neighbours = new GameObject[6];
 		GameObject leftNeighbour = GameObject.Find("Hex_" + (x-1) + "_" + y);
 		GameObject rightNeighbour = GameObject.Find("Hex_" + (x+1) + "_" + y);
+		Neighbours [0] = leftNeighbour;
+		Neighbours [1] = rightNeighbour;
 
 		if (y % 2 == 0) {
 			GameObject upperLeftNeighbour = GameObject.Find ("Hex_" + (x-1) + "_" + (y+1));
 			GameObject upperRightNeighbour = GameObject.Find ("Hex_" + x + "_" + (y+1));
 			GameObject lowerLeftNeighbour = GameObject.Find ("Hex_" + (x-1) + "_" + (y-1));
 			GameObject lowerRightNeighbour = GameObject.Find ("Hex_" + x + "_" + (y-1));
+			Neighbours [2] = upperLeftNeighbour;
+			Neighbours [3] = upperRightNeighbour;
+			Neighbours [4] = lowerLeftNeighbour;
+			Neighbours [5] = lowerRightNeighbour;
 		} else {
 			GameObject upperLeftNeighbour = GameObject.Find ("Hex_" + x + "_" + (y+1));
 			GameObject upperRightNeighbour = GameObject.Find ("Hex_" + (x+1) + "_" + (y+1));
-			GameObject lowerLeftNeighbour = GameObject.Find ("Hex_" + x + "_" + (y+1));
+			GameObject lowerLeftNeighbour = GameObject.Find ("Hex_" + x + "_" + (y-1));
 			GameObject lowerRightNeighbour = GameObject.Find ("Hex_" + (x+1) + "_" + (y-1));
+			Neighbours [2] = upperLeftNeighbour;
+			Neighbours [3] = upperRightNeighbour;
+			Neighbours [4] = lowerLeftNeighbour;
+			Neighbours [5] = lowerRightNeighbour;
 		}
 
-		return null;
+		return Neighbours;
 	}
 
 }
