ddc8ab89bce1081915da2c78df45a7890616acf4
JihaneAb <jihane.ababou@gmail.com>
2016-10-20 16:18:33 +0200

    fix some bugs

--- Assets/Scripts/Map_Jihane.cs
+++ Assets/Scripts/Map_Jihane.cs
@@ -26,6 +26,7 @@ public class Map_Jihane : MonoBehaviour
     List<GameObject> GroupLand = new List<GameObject>();
     List<GameObject> GroupNeighbours = new List<GameObject>();
     Vector3 unitycord = new Vector3(0, 0, 0);
+    GameObject land_go;
 
     // Use this for initialization
     void Start()
@@ -54,6 +55,10 @@ public class Map_Jihane : MonoBehaviour
                 hex_go.GetComponent<Sea>().x = x;
                 hex_go.GetComponent<Sea>().y = y;
 
+                //set the type of hexagon
+                hex_go.GetComponent<Sea>().gs_type = "Sea";
+
+
                 // set the hex in a parent component, parent this hex to the map object
                 hex_go.transform.SetParent(this.transform);
 
@@ -71,8 +76,9 @@ public class Map_Jihane : MonoBehaviour
             ordonnes.Add(y);
         }
 
+        
         // We stat to generate some land
-        for (int a = 0; a < nbCasesRemplinit + 1; a++)
+        for (int a = 0; a < nbCasesRemplinit; a++)
         {
             GameObject remplacable = GameObject.Find("Hex_" + abcisses[a] + "_" + ordonnes[a]);
             unitycord = remplacable.transform.position;
@@ -82,6 +88,7 @@ public class Map_Jihane : MonoBehaviour
             hex_go.name = "Hex_" + abcisses[a] + "_" + ordonnes[a];
             hex_go.GetComponent<Land>().x = abcisses[a];
             hex_go.GetComponent<Land>().y = ordonnes[a];
+            hex_go.GetComponent<Land>().gs_type = "Land";
             hex_go.transform.SetParent(this.transform);
             hex_go.isStatic = true;
             List<GameObject> Neighbours = hex_go.GetComponent<Hex>().getNeighbours();
@@ -89,89 +96,96 @@ public class Map_Jihane : MonoBehaviour
             var nonremplacable = rand.Next(0, Neighbours.Count);
             Neighbours.RemoveAt(nonremplacable);
             FirstStep = Neighbours;
-            Debug.Log(FirstStep.Count);
+
             for (int i = 0; i < FirstStep.Count; i++)
             {
-                //if (FirstStep[i] != LAAAND) { JE FAIS LA SUITE}
-                var abs = FirstStep[i].GetComponent<Hex>().x;
-                var ord = FirstStep[i].GetComponent<Hex>().y;
-                unitycord = FirstStep[i].transform.position;
-                Destroy(FirstStep[i]);
-                GameObject land_go = (GameObject)Instantiate(landPrefab, unitycord, Quaternion.identity);
-                land_go.name = "Hex_" + abs + "_" + ord;
-                land_go.GetComponent<Land>().x = abs;
-                land_go.GetComponent<Land>().y = ord;
-                land_go.transform.SetParent(this.transform);
-                land_go.isStatic = true;
-                //NextNeigbours[i,] = land_go.GetComponent<Hex>().getNeighbours();
-                //Debug.Log(NextNeigbours);
+                if (FirstStep[i].GetComponent<Hex>().gs_type == "Sea")
+                {
+
+                    var abs = FirstStep[i].GetComponent<Hex>().x;
+                    var ord = FirstStep[i].GetComponent<Hex>().y;
+                    unitycord = FirstStep[i].transform.position;
+                    Destroy(FirstStep[i]);
+                    land_go = (GameObject)Instantiate(landPrefab, unitycord, Quaternion.identity);
+                    land_go.name = "Hex_" + abs + "_" + ord;
+                    land_go.GetComponent<Land>().x = abs;
+                    land_go.GetComponent<Land>().y = ord;
+                    land_go.GetComponent<Land>().gs_type = "Land";
+                    land_go.transform.SetParent(this.transform);
+                    land_go.isStatic = true;
+                    //NextNeigbours[i,] = land_go.GetComponent<Hex>().getNeighbours();
+                    //Debug.Log(NextNeigbours);
+                }
                 List<GameObject> NextNeighbours = land_go.GetComponent<Hex>().getNeighbours();
-                var k = 0;
-                while (k < 3)
-                {
-                    var Nextnonremplacable = rand.Next(0, NextNeighbours.Count);
-                    NextNeighbours.RemoveAt(Nextnonremplacable);
-                    k = k + 1;
-                }
-                for (var j = 0; j < NextNeighbours.Count; j++)
-                {
-                    //if (FirstStep[i] != LAAAND) { JE FAIS LA SUITE}
-                    var Nextabs = NextNeighbours[j].GetComponent<Hex>().x;
-                    var Nextord = NextNeighbours[j].GetComponent<Hex>().y;
-                    unitycord = NextNeighbours[j].transform.position;
-                    Destroy(NextNeighbours[j]);
-                    GameObject Next_land_go = (GameObject)Instantiate(landPrefab, unitycord, Quaternion.identity);
-                    Next_land_go.name = "Hex_" + Nextabs + "_" + Nextord;
-                    Next_land_go.GetComponent<Land>().x = Nextabs;
-                    Next_land_go.GetComponent<Land>().y = Nextord;
-                    Next_land_go.transform.SetParent(this.transform);
-                    Next_land_go.isStatic = true;
-                }
+
+                    var k = 0;
+                    while (k < 3)
+                    {
+                        if(NextNeighbours != null)
+                        { 
+                            var Nextnonremplacable = rand.Next(0, NextNeighbours.Count);
+                            NextNeighbours.RemoveAt(Nextnonremplacable);
+                        }
+                        k = k + 1;
+                    }
+                    for (var j = 0; j < NextNeighbours.Count; j++)
+                    {
+                        if (FirstStep[i].GetComponent<Hex>().gs_type == "Sea")
+                        {
+                            var Nextabs = NextNeighbours[j].GetComponent<Hex>().x;
+                            var Nextord = NextNeighbours[j].GetComponent<Hex>().y;
+                            unitycord = NextNeighbours[j].transform.position;
+                            Destroy(NextNeighbours[j]);
+                            GameObject Next_land_go = (GameObject)Instantiate(landPrefab, unitycord, Quaternion.identity);
+                            Next_land_go.name = "Hex_" + Nextabs + "_" + Nextord;
+                            Next_land_go.GetComponent<Land>().x = Nextabs;
+                            Next_land_go.GetComponent<Land>().y = Nextord;
+                            hex_go.GetComponent<Land>().gs_type = "Land";
+                            Next_land_go.transform.SetParent(this.transform);
+                            Next_land_go.isStatic = true;
+                        }
+                    }
+                
             }
-        }
-        //Vérification Map
-        var verif = false;
-        var l = 0;
-        var m = 0;
-        while (verif != true && l < width)
-        {
-            m = 0;
-            while (verif != true && m < height)
+            /*
+            //Vérification Map
+            var verif = false;
+            var l = 0;
+            var m = 0;
+            while (verif != true && l < width)
             {
-                //if hex l_m est un sea
-                //{
-                //GroupLand.Add(Hex(l_m));
-                //verif = true;
-                //hex(l_m).tag = true;
-                //}
+                m = 0;
+                while (verif != true && m < height)
+                {
+                    //if hex l_m est un sea
+                    //{
+                    //GroupLand.Add(Hex(l_m));
+                    //verif = true;
+                    //hex(l_m).tag = true;
+                    //}
+                }
             }
-        }
-        while (GroupLand.Count > 0)
-        {
-
-            //GroupNeighbours = GroupLand[0].GetComponent<Hex>().getNeighbours();
-
-            for (var j = 0; j < GroupNeighbours.Count; j++)
+            while (GroupLand.Count > 0)
             {
-                //if GroupNeighbours[j]==sea && tag ==false
-                //{
-                //GroupLand.add(GroupNeighbours[j]);
-                //GroupNeighbours[j].tag = true;
-                //}
+
+                //GroupNeighbours = GroupLand[0].GetComponent<Hex>().getNeighbours();
+
+                for (var j = 0; j < GroupNeighbours.Count; j++)
+                {
+                    //if GroupNeighbours[j]==sea && tag ==false
+                    //{
+                    //GroupLand.add(GroupNeighbours[j]);
+                    //GroupNeighbours[j].tag = true;
+                    //}
+                }
+
             }
+            */
+
+
 
         }
 
-
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Update is called once per frame
     }
 }
-
-
