72c35c69aa236264a6d902cf677d75484b063aaa
Elektro Korobase <korobase.elektro@gmail.com>
2016-10-26 10:05:48 +0200

    Lancement de la carte + configuration taille map

--- Assets/Scripts/HUD/mainMenu/LaunchScript.cs
+++ Assets/Scripts/HUD/mainMenu/LaunchScript.cs
@@ -1,12 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-public class LaunchScript : MonoBehaviour {
-
-	public void LaunchGame() {
-		
-		SceneManager.LoadScene(1);
-	
-	}
-}
+﻿
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class MapManager : MonoBehaviour {
 	/* The MapManager is a class which will be used to pass Map informations between Scenes
 	 * For example, it can be called to the Map scene at anytime to know :
@@ -9,6 +10,8 @@ public class MapManager : MonoBehaviour {
 	 * * Etc...
 	 * */
 	// TODO have a loader in the Main Menu to create the MapManager ( see ://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/ )
+
+
 
 	public static MapManager instance = null;
 	// The magic works here : You can use the singleton just by indicating MapManager.instance
@@ -20,13 +23,18 @@ public class MapManager : MonoBehaviour {
 	public int GroundFrequencyParameter = 2;
 	public int PortFrequencyParameter = 2;
 	public int TreasureFrequencyParameter = 2;
+	int MapX = 0;
+	int MapY = 0;
 	//public int Parameter =;
 	//public int Parameter =;
+	public GameObject hexPrefab;
+	public GameObject landPrefab;
+	public GameObject seaPrefab;
+	public GameObject mapPrefab;
 
 
 
-
-	// Use this before initialization
+	// Use this before initialization (and between loading Maps)
 	void Awake() {
 		//Check if instance already exists
 		if (instance == null)
@@ -36,7 +44,7 @@ public class MapManager : MonoBehaviour {
 
 		//If instance already exists and it's not this:
 		else if (instance != this)
-
+			
 			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a MapManager.
 			Destroy(gameObject);    
 
@@ -58,6 +66,36 @@ public class MapManager : MonoBehaviour {
 		// 5: Gigantesque
 		// 6: Personnalisé
 		MapWidthParameter = WidthType;
+		switch (WidthType) {
+		case 0:
+			MapX = 40;
+			MapY = 24;
+			break;
+		case 1:
+			MapX = 56;
+			MapY = 36;
+			break;
+		case 2:
+			MapX = 66;
+			MapY = 42;
+			break;
+		case 3:
+			MapX = 80;
+			MapY = 52;
+			break;
+		case 4:
+			MapX = 104;
+			MapY = 64;
+			break;
+		case 5:
+			MapX = 128;
+			MapY = 80;
+			break;
+		case 6:
+			MapX = 5;
+			MapY = 5;
+			break;
+		}
 	}
 
 	public void DifficultyDropdownCallback(int DifficultyType) {
@@ -91,4 +129,32 @@ public class MapManager : MonoBehaviour {
 		// 4: Très élevée
 		TreasureFrequencyParameter = TreasureFrequencyType;
 	}
+
+
+	private Map initMap() {
+		GameObject newObject = Instantiate(mapPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		Map mapSettings = newObject.GetComponent<Map>();
+		//do additional initialization steps here
+
+		mapSettings.hexPrefab = hexPrefab;
+		mapSettings.landPrefab = landPrefab;
+		mapSettings.seaPrefab = seaPrefab;
+
+		mapSettings.height = MapX;
+		mapSettings.width = MapY;
+
+		return mapSettings;
+	}
+
+	private void initWorld() {
+		Map worldMap = initMap ();
+		GameObject mouseSettings = GameObject.Find ("mouseManager");
+		((MouseManager)mouseSettings.GetComponent<MouseManager> ()).map = worldMap;
+	}
+
+	void OnLevelWasLoaded() {
+		Debug.Log ("Chargement de la map....");
+		initWorld ();
+	}
+
 }
