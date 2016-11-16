fb8c1f8de9c8d819286327aa961fbe8a2bfed2f7
A-Picogna <apicogna@enssat.fr>
2016-11-16 14:14:34 +0100

    Rangement code

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -23,35 +23,38 @@ public class Map : MonoBehaviour {
 	// size of map in terms of numer of hexagon
 	public int width;
 	public int height;
-	public int size;
-	int nbCasesRemplinit;
-	float xOffset = Mathf.Sqrt(3)/2;
-	float zOffset = 0.75f;
-	bool mapFausse;
-	List<int> abcisses;
-	List<int> ordonnes;
-	List<Node> FirstStep;
-	List<Node> GroupSea;
-	List<Node> GroupNeighbours;
-	Vector3 worldCoord;
-	GameObject land_go;
-	System.Random rand;
-    List<Node> GroupLand;
-    List<List<Node>> GroupListPossibleHarbor;
-	int regenerateCount;
+	public int nbCasesRemplinit;
 	public int nombreCasesTreasure;
 	public int nombreCasesFood;
-	Vector3 worldCoordFood;
-	Vector3 worldCoordTreasure;
 	public int tresorMin;
 	public int tresorMax;
 	public int foodQuantityMax;
 	public int foodQuantityMin;
 
+	int size;
+	int regenerateCount;
+	float xOffset = Mathf.Sqrt(3)/2;
+	float zOffset = 0.75f;
+	bool mapFausse;
+
+	List<int> abcisses;
+	List<int> ordonnes;
+	List<Node> FirstStep;
+	List<Node> GroupSea;
+	List<Node> GroupNeighbours;
+	List<Node> GroupLand;
+	List<List<Node>> GroupListPossibleHarbor;
+
+	Vector3 worldCoordFood;
+	Vector3 worldCoordTreasure;
+	Vector3 worldCoord;
+
+	GameObject land_go;
+	System.Random rand;
+
     // Use this for initialization
     void Start () {
 
-		nbCasesRemplinit = 25;
 		size = width * height;
 		rand = new System.Random();
         GroupLand = new List<Node>();
@@ -61,12 +64,6 @@ public class Map : MonoBehaviour {
 		regenerateCount = 1;
 		worldCoordFood = new Vector3(0, 0, 0);
 		worldCoordTreasure = new Vector3(0, 0, 0);
-		nombreCasesTreasure = 50;
-		nombreCasesFood = 1400;
-		tresorMin = 200;
-		tresorMax = 1000;
-		foodQuantityMax = 50;
-		foodQuantityMin = 20;
 
 		// Init map
 		InitializeMap();
