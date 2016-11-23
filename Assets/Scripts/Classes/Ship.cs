8e1f37e0b78999454f3ed68967ffe92e22d26fbb
Yiserot <yiserot@mail.com>
2016-11-23 19:20:45 +0100

    UI: ships UI updated after actions

--- Assets/Scripts/Classes/Ship.cs
+++ Assets/Scripts/Classes/Ship.cs
@@ -27,6 +27,7 @@ public class Ship : MonoBehaviour {
 	bool dead = false;
 	private string shipName;
 	private List<CrewMember> crew = new List<CrewMember>();
+	private PanelHandler panelHandler;
 	public Vector3 destination;
 	public AudioClip shipMovingSound;
 
@@ -75,12 +76,14 @@ public class Ship : MonoBehaviour {
 		} else {
 			currentPath = null;
 		}
+		panelHandler.updateShip ();
 	}
 
 	public void Die(){
 		dead = true;
 		Destroy (this.GetComponentInChildren<MeshCollider> ());
 		StartCoroutine (Sink ());
+		panelHandler.updateShip ();
 	}
 
 	public void RefuelEnergy(){
@@ -89,6 +92,7 @@ public class Ship : MonoBehaviour {
 			res += c.EnergyQuantity;
 		}
 		energyQuantity = res;
+		panelHandler.updateShip ();
 	}
 
 	IEnumerator Sink (){
@@ -148,6 +152,7 @@ public class Ship : MonoBehaviour {
 				energyQuantity -= 5;
 			}
 		}
+		panelHandler.updateShip ();
 	}
 
 	public bool AtFilibusterRange(Ship target){
@@ -305,6 +310,7 @@ public class Ship : MonoBehaviour {
 			}
 		} while (takingDamages);
 		UpdateShipHp ();
+		panelHandler.updateShip ();
 	}
 
 	public void UpdateShipHp(){
@@ -341,6 +347,7 @@ public class Ship : MonoBehaviour {
 			Destroy (target.Treasure_go);
 			target.RemoveTreasure();
 			energyQuantity -= 3;
+			panelHandler.updateShip ();
 			return true;
 		}
 		else{
@@ -364,7 +371,7 @@ public class Ship : MonoBehaviour {
 			//decrement energyQuantity, to check, debug value
 			//Or set it to 0 to make the ship unable to move
 			energyQuantity -= 5;
-
+			panelHandler.updateShip ();
 			return true;
 		}
 		else{ 
@@ -376,11 +383,15 @@ public class Ship : MonoBehaviour {
 		crew.Add(member);
         hp += member.Lp;
         energyQuantity += member.EnergyQuantity;
+		if (panelHandler)
+			panelHandler.updateShip ();
     }
 
 	public void removeCrewMember(CrewMember member){
 		crew.Remove(member);
         hp -= member.Lp;
+		if (panelHandler)
+			panelHandler.updateShip ();
 	}
 
 
@@ -477,6 +488,9 @@ public class Ship : MonoBehaviour {
 	// ====================
 	// ====================
 
-
-
+	public PanelHandler PanelHandler
+	{
+		get { return panelHandler; }
+		set { panelHandler = value; }
+	}
 }
