b964a7f70c0a542ebb0988f70ea3b97c15688bdc
Yiserot <yiserot@mail.com>
2016-11-24 22:28:00 +0100

    UI: working version + trade beginning

---
+++ Assets/Scripts/HUD/inGame/TradePanel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class TradePanel : MonoBehaviour {
+	public GameObject panel;
+	public Button button;
+	public Text header;
+
+	private Lang lang;
+
+	// Use this for initialization
+	void Start () {
+		panel.SetActive (false);
+		button.onClick.AddListener (hidePanel);
+		refreshPanel ();
+	}
+
+	public void refreshPanel() {
+		lang = new Lang(Path.Combine(Application.dataPath, GlobalVariables.pathLang), GlobalVariables.currentLang);
+		header.text = lang.getString("trade");
+	}
+	
+	public void showPanel() {
+		panel.SetActive (true);
+	}
+
+	public void hidePanel() {
+		panel.SetActive (false);
+	}
+}
