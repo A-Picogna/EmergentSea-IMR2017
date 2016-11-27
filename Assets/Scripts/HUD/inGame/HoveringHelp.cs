2eb763b6d40e3dd1c5376794649c63272944aaf8
Yiserot <yiserot@mail.com>
2016-11-27 23:11:04 +0100

    UI: first contextual help in trade panel

---
+++ Assets/Scripts/HUD/inGame/HoveringHelp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class HoveringHelp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public GameObject panelHelp;
+
+	// Use this for initialization
+	void Start () {
+		panelHelp.SetActive(false);
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		panelHelp.SetActive(true);
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		panelHelp.SetActive(false);
+	}
+}
