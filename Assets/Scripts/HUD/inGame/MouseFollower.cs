99b8dcca930eebd6253bb2c35b17078e787f6721
Yiserot <yiserot@mail.com>
2016-11-30 13:35:31 +0100

    UI: MouseFollower correction

--- Assets/Scripts/HUD/inGame/MouseFollower.cs
+++ Assets/Scripts/HUD/inGame/MouseFollower.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MouseFollower : MonoBehaviour {
 	private Canvas canvas;
-	public int type;
+	public int positionMode;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +13,11 @@ public class MouseFollower : MonoBehaviour {
 	public void refreshPosition() {
 		Vector2 pos;
 		RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos);
-		if (type == 0) {
+		if (positionMode == 0) {
 			pos.x += 75;
-		} else if (type == 1) {
-			pos.x += 100;
-			pos.y += 100;
+		} else if (positionMode == 1) {
+			pos.x -= 20;
+			pos.y += 20;
 		}
 		transform.position = canvas.transform.TransformPoint(pos);
 	}			
