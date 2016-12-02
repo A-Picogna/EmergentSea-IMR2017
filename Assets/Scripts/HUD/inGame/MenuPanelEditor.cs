0530d8b71e8497ab5a706182c74111a9846fedb2
Yiserot <yiserot@mail.com>
2016-12-02 17:35:50 +0100

    UI: no bug no pain

---
+++ Assets/Scripts/HUD/inGame/MenuPanelEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class MenuPanelEditor : MonoBehaviour {
+	public Text menuHeader;
+	public Text resume;
+	public Text save;
+	public Text quit;
+	private Lang lang;
+
+	void Start () {
+		lang = new Lang(Path.Combine(Application.dataPath, GlobalVariables.pathLang), GlobalVariables.currentLang);
+		menuHeader.text = lang.getString("menu");
+		resume.text = lang.getString("resume");
+		save.text = lang.getString("save");
+		quit.text = lang.getString("quit");
+	}
+}
+
