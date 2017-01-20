04e661d1d8578e7fd5f637d4dcc58fdf5cfefda7
Alexandre Picogna <apicogna@enssat.fr>
2017-01-20 10:36:44 +0100

    remove test

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -388,16 +388,8 @@ public class GameManager : MonoBehaviour {
 		Ship ship = ship_go.GetComponent<Ship> ();
 		ship.Owner = player;
 		ship.addCrewMember(new Filibuster());
-		ship.addCrewMember(new Filibuster());
-		//if (player.Type.Equals ("IA")) {
-			ship.addCrewMember(new PowderMonkey());
-			ship.addCrewMember(new Conjurer());
-			ship.addCrewMember(new PowderMonkey());
-			ship.addCrewMember(new Conjurer());
-		//}
-		foreach (CrewMember cm in ship.Crew) {
-			cm.Lp = rand.Next (1, cm.LpMax);
-		}
+		ship.addCrewMember(new PowderMonkey());
+		ship.addCrewMember(new Conjurer());
 		player.Fleet.Add (ship);
 		mouseManager.map.graph [x, y].isWalkable = false;
 		GameObject.Find("Hex_" + x + "_" + y).GetComponent<Sea>().ShipContained = ship;
