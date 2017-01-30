54887a13079345b43a20e10c464a53dd9f8dc5a6
Alexandre Picogna <apicogna@enssat.fr>
2017-01-30 23:02:00 +0100

    small add

--- Assets/Scripts/Classes/Ship.cs
+++ Assets/Scripts/Classes/Ship.cs
@@ -412,11 +412,11 @@ public class Ship : MonoBehaviour {
 	public void GiveXP(int crewType){
 		foreach (CrewMember c in crew) {
 			if (c.Type == crewType){
-				c.gainXP (15);
+				c.gainXP (25);
 			}
 			// We also give xp to the admiral, code 0
 			if (c.Type == 0) {
-				c.gainXP (10);
+				c.gainXP (25);
 			}
 		}
 	}
